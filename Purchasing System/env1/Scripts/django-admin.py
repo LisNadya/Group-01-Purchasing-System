2eaed8d1ceafdd7197b8af637d0dbbc3f307ba69
AsilaAjwa-1151104536-YupItsMe <60035300+AsilaAjwa-1151104536-YupItsMe@users.noreply.github.com>
2020-02-03 15:29:53 +0800

    Menu button added at both app layout and tests

---
+++ Purchasing System/env1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Asila Ajwa\source\repos\Group-01-Purchasing-System\Purchasing System\env1\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
